ae05464b2c58358e3987f671d8e824d33734d458
shresthgoel77-png <shresthgoel77@gmail.com>
2026-07-19 20:46:55 +0530

    added the alembic logic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
