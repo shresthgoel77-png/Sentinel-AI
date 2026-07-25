69630a1049ada14f7814e80eaa1fc40564c57b5e
shresthgoel77-png <shresthgoel77@gmail.com>
2026-07-26 00:37:56 +0530

    created run_test.py

--- backend/run_tests.py
+++ backend/run_tests.py
@@ -4,3 +4,5 @@ with open("pytest_utf8.log", "w", encoding="utf-8") as f:
     result = subprocess.run([".\\venv\\Scripts\\python.exe", "-m", "pytest", "tests/test_gateway.py", "--tb=short"], capture_output=True, text=True)
     f.write(result.stdout)
     f.write(result.stderr)
+
+#This is only a test file 
