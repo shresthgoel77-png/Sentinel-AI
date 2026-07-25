34cac2eaef3d4d1c18085b2956d126567973c6cb
shresthgoel77-png <shresthgoel77@gmail.com>
2026-07-26 00:13:35 +0530

    deleted some files

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ const api = axios.create({
 // Global request interceptor injecting auth seamlessly without repeating in fetch blocks
 api.interceptors.request.use((config) => {
     // Note: To expand into production JWT, we can pull token from Context mapping here
-    config.headers.Authorization = `Bearer sk_sentinel_demo_key`;
+    config.headers.Authorization = `Bearer api_key_sentinel_demo`;
     return config;
 });
 
